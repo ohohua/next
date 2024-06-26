a30680ceb0d068282d80eeab36a6cec3623eb93a
ohohua <ohohua@163.com>
2024-06-26 18:07:38 +0800

    first commit

---
+++ app/@team/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return (
+    <div className="h-60 flex-1 rounded-xl bg-indigo-500 text-white flex items-center justify-center">
+      team
+    </div>
+  )
+}
+
+export default Page
